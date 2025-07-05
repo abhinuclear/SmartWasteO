6413c00ec2b9fffdba7ad23c0e9047ceb0b60d0f
abhinuclear <mr.abhinuclear@gmail.com>
2025-07-05 10:56:59 +0530

    Initial commit

---
+++ app/src/main/java/com/project/smartwasteo/FirstScreen.kt
@@ -0,0 +1,20 @@
+package com.project.smartwasteo
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun UserSelection(){
+
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+
+    ){
+        Button(OnClick) { }
+    }
+
+}
