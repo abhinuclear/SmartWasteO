c759b134d43d335e8f365381408d07b237def893
abhinuclear <mr.abhinuclear@gmail.com>
2025-07-09 17:40:11 +0530

    nav

--- app/src/main/java/com/project/smartwasteo/AppNavigation.kt
+++ app/src/main/java/com/project/smartwasteo/AppNavigation.kt
@@ -1,2 +1,28 @@
 package com.project.smartwasteo
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.project.smartwasteo.authority.LoginAuthority
+import com.project.smartwasteo.worker.WorkerLogin
+
+
+@Composable
+fun AppNavigation(modifier: Modifier= Modifier,authViewModel: AuthViewModel){
+    val navController= rememberNavController()
+    NavHost(navController=navController, startDestination = "firstScreen", builder = {
+        composable ("firstScreen"){
+            FirstScreen(modifier,navController)
+        }
+        composable("loginauthority"){
+            LoginAuthority(modifier,navController,authViewModel)
+        }
+        composable ("workerauthority"){
+            WorkerLogin(modifier,navController,authViewModel)
+        }
+    })
+
+}
+
---
+++ app/src/main/java/com/project/smartwasteo/authority/LoginAuthority.kt
@@ -0,0 +1,14 @@
+package com.project.smartwasteo.Authority
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import com.project.smartwasteo.AuthViewModel
+
+@Composable
+fun LoginAuthority(
+    modifier: Modifier, navController: NavController, authViewModel: AuthViewModel
+){
+
+}
