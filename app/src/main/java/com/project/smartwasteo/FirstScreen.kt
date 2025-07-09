21bd85a8cb8187cda5d01ac7957bf3833e7cbbe2
abhinuclear <mr.abhinuclear@gmail.com>
2025-07-09 17:41:31 +0530

    authority adn worker login

--- app/src/main/java/com/project/smartwasteo/FirstScreen.kt
+++ app/src/main/java/com/project/smartwasteo/FirstScreen.kt
@@ -1,20 +1,96 @@
 package com.project.smartwasteo
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+//import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+//import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+//import androidx.compose.ui.semantics.SemanticsActions.OnClick
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 
 @Composable
-fun UserSelection(){
+fun FirstScreen(modifier: Modifier,navController: NavController) {
 
-    Column (
+    Box(
         modifier = Modifier
             .fillMaxSize()
+            .background(Color.Yellow)
 
-    ){
-        Button(OnClick) { }
+    ) {
+        Column(
+            modifier = Modifier
+                .align(Alignment.Center),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(
+                onClick = {
+                    navController.navigate("loginauthority")
+                },
+                shape = RoundedCornerShape(16.dp),
+                colors = ButtonDefaults.buttonColors(contentColor = Color.Yellow),
+                modifier = modifier
+                    .size(150.dp)
+
+            ) {
+                Text(
+                    fontSize = 24.sp, text = "Authority"
+                )
+            }
+            Spacer(modifier=modifier.height(16.dp))
+
+            Button(
+                onClick = {
+                    navController.navigate("workerauthority")
+                },
+                shape = RoundedCornerShape(16.dp),
+                colors = ButtonDefaults.buttonColors(contentColor = Color.Yellow),
+                modifier = modifier
+
+                    .size(150.dp)
+
+
+            ) {
+                Text(fontSize = 24.sp, text = "Worker")
+            }
+        }
+
     }
 
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
