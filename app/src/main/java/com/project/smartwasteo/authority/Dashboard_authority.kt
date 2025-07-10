8dd7b218067125b6981ee191e2ca7edb28594117
abhinuclear <mr.abhinuclear@gmail.com>
2025-07-10 19:25:37 +0530

    dashboard authority

--- app/src/main/java/com/project/smartwasteo/authority/Dashboard_authority.kt
+++ app/src/main/java/com/project/smartwasteo/authority/Dashboard_authority.kt
@@ -1,2 +1,19 @@
 package com.project.smartwasteo.authority
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import com.project.smartwasteo.AuthViewModel
+
+
+    @Composable
+    fun Dashboard_authority(
+        modifier: Modifier = Modifier,
+        navController: NavController,
+        authViewModel: AuthViewModel
+    ) {
+        val complaintViewModel: ComplaintViewModel = viewModel()
+        ComplaintScreen(viewModel = complaintViewModel)
+    }
