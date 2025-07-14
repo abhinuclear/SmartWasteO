71e18b349c6d2a9e8af3163f920deeaaf1a72ca1
abhinuclear <mr.abhinuclear@gmail.com>
2025-07-14 22:06:29 +0530

    ch

--- app/src/main/java/com/project/smartwasteo/worker/Dashboard_worker.kt
+++ app/src/main/java/com/project/smartwasteo/worker/Dashboard_worker.kt
@@ -1,2 +1,13 @@
 package com.project.smartwasteo.worker
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import com.project.smartwasteo.AuthViewModel
+
+@Composable
+fun Dashboard_worker(    modifier: Modifier, navController: NavController, authViewModel: AuthViewModel
+){
+    Text("worker page")
+}
