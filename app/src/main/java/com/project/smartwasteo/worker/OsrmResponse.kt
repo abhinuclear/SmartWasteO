b78e887a699184ec1bd1c052ed085a6c6029632b
abhinuclear <mr.abhinuclear@gmail.com>
2025-07-15 01:57:18 +0530

    route optimization

--- app/src/main/java/com/project/smartwasteo/worker/OsrmResponse.kt
+++ app/src/main/java/com/project/smartwasteo/worker/OsrmResponse.kt
@@ -1,3 +1,25 @@
 package com.project.smartwasteo.worker
 
-data class OsrmResponse()
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class OSRMTripResponse(
+    val trips: List<Trip>,
+    val waypoints: List<Waypoint>
+)
+
+@Serializable
+data class Trip(
+    val geometry: String
+)
+
+@Serializable
+data class Waypoint(
+    val location: List<Double>, // [lon, lat]
+    val waypoint_index: Int
+)
+
+data class RoutePoint(
+    val latitude: Double,
+    val longitude: Double
+)
