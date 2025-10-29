672c7b7b13c4c63eb6042cb1d67c8d4d866a05f2
abhinuclear <mr.abhinuclear@gmail.com>
2025-10-29 10:36:37 +0530

    back navigate to first

--- app/src/main/java/com/project/smartwasteo/AppNavigation.kt
+++ app/src/main/java/com/project/smartwasteo/AppNavigation.kt
@@ -1,10 +1,12 @@
 package com.project.smartwasteo
 
+//import Dashboard_worker
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.project.smartwasteo.authority.ComplaintScreen
 import com.project.smartwasteo.authority.Dashboard_authority
 import com.project.smartwasteo.authority.LoginAuthority
 import com.project.smartwasteo.worker.Dashboard_worker
@@ -21,15 +23,19 @@ fun AppNavigation(modifier: Modifier= Modifier,authViewModel: AuthViewModel){
         composable("loginauthority"){
             LoginAuthority(modifier,navController,authViewModel)
         }
-        composable ("workerauthority"){
-            WorkerLogin(modifier,navController,authViewModel)
-        }
-        composable ("dashboardAuthority"){
+        composable("dashboardAuthority"){
             Dashboard_authority(modifier,navController,authViewModel)
+        }
+        composable ("workerlogin"){
+            WorkerLogin(modifier,navController,authViewModel)
         }
         composable ("dashboardWorker"){
             Dashboard_worker(modifier,navController,authViewModel)
         }
+        composable("complaintScreen") {
+            ComplaintScreen(navController)
+        }
+
     })
 
 }
