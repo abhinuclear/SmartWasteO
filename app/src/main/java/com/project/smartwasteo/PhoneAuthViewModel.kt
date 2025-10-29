fff63e8e2afd487beaa771e6d077ca302257103f
abhinuclear <mr.abhinuclear@gmail.com>
2025-10-29 10:38:05 +0530

    auth simplification

--- app/src/main/java/com/project/smartwasteo/PhoneAuthViewModel.kt
+++ app/src/main/java/com/project/smartwasteo/PhoneAuthViewModel.kt
@@ -16,7 +16,9 @@ class PhoneAuthViewModel : ViewModel() {
     var verificationId = mutableStateOf("")
     var otpSent = mutableStateOf(false)
     var isLoading = mutableStateOf(false)
+
     var errorMessage = mutableStateOf<String?>(null)
+
 
     private val auth: FirebaseAuth = FirebaseAuth.getInstance()
 
@@ -48,11 +50,14 @@ class PhoneAuthViewModel : ViewModel() {
     }
 
     fun verifyOtp(code: String, onSuccess: () -> Unit) {
+      //  _isLoading.value = true
+       // _errorMessage.value = null
         val credential = PhoneAuthProvider.getCredential(verificationId.value, code)
         signInWithPhoneAuthCredential(credential, onSuccess)
     }
 
     private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential, onSuccess: (() -> Unit)? = null) {
+
         auth.signInWithCredential(credential)
             .addOnCompleteListener { task ->
                 isLoading.value = false
